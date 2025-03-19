c2515ffd1f8fce1f6945f47691ff42f5b09c16a2
Ian Hornik <yiliansource@gmail.com>
2025-03-19 18:19:44 +0100

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,119 @@
+import {
+    Grid,
+    GridIndex,
+    gridSet,
+    isGridConsistent,
+    isGridSolved,
+    parseGridIndex,
+    transposeSubgrid,
+} from "./lib/matrix";
+import clsx from "clsx";
+import { motion } from "motion/react";
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+export default function App() {
+    const [grid, setGrid] = useState<Grid<number>>({});
+
+    const isInvalid = useMemo(() => !isGridConsistent(grid), [grid]);
+    const isSolved = useMemo(() => isGridSolved(grid), [grid]);
+
+    useEffect(() => {
+        handleRegenerate();
+    }, []);
+
+    const handleRegenerate = () => {
+        const l = 6;
+
+        const initialGrid: Grid<number> = {};
+        const widths = [...Array(l).keys()].map(() => Math.floor(Math.random() * l)).sort((a, b) => b - a);
+
+        let n = Date.now();
+
+        for (let j = 0; j < l; j++) {
+            for (let i = 0; i < widths[j]; i++) {
+                gridSet(initialGrid, [j, i], n++);
+            }
+        }
+
+        setGrid(initialGrid);
+    };
+
+    const handleGridClick = useCallback(
+        (index: GridIndex) => {
+            const newGrid = transposeSubgrid(grid, index);
+            setGrid(newGrid);
+
+            if (!isGridConsistent(newGrid)) {
+                setTimeout(() => {
+                    setGrid(grid);
+                }, 500);
+            }
+        },
+        [grid],
+    );
+
+    const cellSize = 32;
+    const cellGap = 2;
+
+    const gridWidth = 8;
+    const gridHeight = 8;
+
+    return (
+        <div className="flex min-h-screen flex-col">
+            <header></header>
+            <main className="mx-auto flex w-full max-w-4xl grow">
+                <div className="m-auto flex flex-col items-center gap-6">
+                    <h1 className="mb-4 text-center text-3xl font-bold">Young's Game</h1>
+                    <div
+                        className={clsx(
+                            "relative",
+                            isInvalid && "pointer-events-none cursor-not-allowed opacity-50",
+                            isSolved && "pointer-events-none cursor-not-allowed",
+                        )}
+                        style={{
+                            width: gridWidth * cellSize + (gridWidth - 1) * cellGap,
+                            height: gridHeight * cellSize + (gridHeight - 1) * cellGap,
+                        }}
+                    >
+                        {Object.entries(grid).map(([indexStr, value]) => {
+                            const index = parseGridIndex(indexStr);
+                            const [i, j] = index;
+
+                            return (
+                                <motion.div
+                                    key={value}
+                                    className={clsx(
+                                        "absolute rounded-md",
+                                        !isInvalid && !isSolved && "bg-neutral-700",
+                                        isInvalid && "bg-red-700",
+                                        isSolved && "bg-green-500",
+                                    )}
+                                    onClick={() => handleGridClick(index)}
+                                    style={{
+                                        width: 32 + "px",
+                                        height: 32 + "px",
+                                    }}
+                                    initial={false}
+                                    animate={{
+                                        x: j * (cellSize + cellGap),
+                                        y: i * (cellSize + cellGap),
+                                    }}
+                                    whileHover={{ scale: 1.1 }}
+                                    whileTap={{ scale: 0.9 }}
+                                    layout
+                                ></motion.div>
+                            );
+                        })}
+                    </div>
+                    <button
+                        className="cursor-pointer rounded-sm border bg-neutral-200 px-4 py-2 hover:bg-neutral-300"
+                        onClick={handleRegenerate}
+                    >
+                        Regenerate
+                    </button>
+                </div>
+            </main>
+            <footer></footer>
+        </div>
+    );
+}
---
+++ src/lib/matrix.ts
@@ -0,0 +1,113 @@
+export type Grid<T> = Record<string, T>;
+export type GridIndex = [number, number];
+
+export function gridGet<T>(g: Grid<T>, i: GridIndex) {
+    return g[stringifyGridIndex(i)];
+}
+export function gridSet<T>(g: Grid<T>, i: GridIndex, v: T) {
+    g[stringifyGridIndex(i)] = v;
+}
+export function gridDelete<T>(g: Grid<T>, i: GridIndex) {
+    delete g[stringifyGridIndex(i)];
+}
+
+export function parseGridIndex(s: string): GridIndex {
+    return s.split(",").map(Number) as GridIndex;
+}
+export function stringifyGridIndex(i: GridIndex) {
+    return i.join(",");
+}
+
+export function transposeSubgrid<T>(m: Grid<T>, rel: GridIndex): Grid<T> {
+    const [i0, j0] = rel;
+
+    const newGrid: Grid<T> = {};
+
+    for (const [gridIndexStr, gridValue] of Object.entries(m)) {
+        const gridIndex = parseGridIndex(gridIndexStr);
+        const [i, j] = gridIndex;
+
+        if (i >= i0 && j >= j0) {
+            const transposedGridIndex = transposeIndexRelative(gridIndex, rel);
+            newGrid[stringifyGridIndex(transposedGridIndex)] = gridValue;
+        } else {
+            newGrid[stringifyGridIndex(gridIndex)] = gridValue;
+        }
+    }
+
+    return newGrid;
+}
+
+export function transposeIndex([i, j]: GridIndex) {
+    return [j, i] as GridIndex;
+}
+export function transposeIndexRelative([i, j]: GridIndex, [i0, j0]: GridIndex): GridIndex {
+    return [i0 + j - j0, j0 + i - i0] as GridIndex;
+}
+
+function getGridIndices<T>(grid: Grid<T>): GridIndex[] {
+    return Object.keys(grid).map(parseGridIndex);
+}
+
+export function isGridConsistent<T>(grid: Grid<T>): boolean {
+    const gridIndices = getGridIndices(grid);
+    const indicesByRow = groupByMapped(
+        gridIndices,
+        (c) => c[0].toString(),
+        (c) => c[1],
+    );
+    const rowIndices = Object.keys(indicesByRow).map(Number);
+    const rowWidths = rowIndices.map((rowIndex) => Math.max(...indicesByRow[rowIndex]) + 1);
+
+    for (const rowIndex of rowIndices) {
+        if (indicesByRow[rowIndex].length !== rowWidths[rowIndex]) {
+            return false;
+        }
+    }
+
+    for (let i = 0; i < rowIndices.length - 1; i++) {
+        if (rowWidths[i + 1] > rowWidths[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+export function isGridSolved<T>(grid: Grid<T>): boolean {
+    const gridIndices = getGridIndices(grid);
+    const indicesByRow = groupByMapped(
+        gridIndices,
+        (c) => c[0].toString(),
+        (c) => c[1],
+    );
+    const rowIndices = Object.keys(indicesByRow).map(Number);
+    const rowWidths = rowIndices.map((rowIndex) => Math.max(...indicesByRow[rowIndex]) + 1);
+
+    for (const rowIndex of rowIndices) {
+        if (indicesByRow[rowIndex].length !== rowWidths[rowIndex]) {
+            return false;
+        }
+    }
+
+    for (let i = 0; i < rowIndices.length - 1; i++) {
+        if (rowWidths[i + 1] >= rowWidths[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+const groupByMapped = <T, K>(
+    array: T[],
+    predicate: (value: T, index: number, array: T[]) => string,
+    map: (value: T, index: number, array: T[]) => K,
+) =>
+    array.reduce(
+        (acc, value, index, array) => {
+            (acc[predicate(value, index, array)] ||= []).push(map(value, index, array));
+            return acc;
+        },
+        {} as { [key: string]: K[] },
+    );
