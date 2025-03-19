519c56657afd3280920a9ec9e5da89bd12cdf858
Ian Hornik <yiliansource@gmail.com>
2025-03-19 19:13:55 +0100

    add styling

--- src/app.tsx
+++ src/app.tsx
@@ -50,17 +50,21 @@ export default function App() {
     const gridHeight = 8;
 
     return (
-        <div className="flex min-h-screen flex-col">
+        <div className="flex min-h-screen flex-col bg-neutral-100">
             <header></header>
-            <main className="mx-auto flex w-full max-w-4xl grow">
-                <div className="m-auto flex flex-col items-center gap-6">
-                    <h1 className="mb-4 text-center text-3xl font-bold">Young's Game</h1>
+            <main className="mx-auto flex w-full max-w-4xl grow px-2">
+                <div className="m-auto flex flex-col items-center">
+                    <div className="my-4">
+                        <h1 className="mb-4 text-center text-4xl font-bold">Young's Game</h1>
+                        <p className="text-center leading-7">
+                            Your goal is to rearrange the rows in strictly decreasing order of widths.
+                            <br />
+                            Clicking a cell flips everything to the bottom-right of it. This may not break sorting or
+                            create gaps.
+                        </p>
+                    </div>
                     <div
-                        className={clsx(
-                            "relative",
-                            isInvalid && "pointer-events-none cursor-not-allowed opacity-50",
-                            isSolved && "pointer-events-none cursor-not-allowed",
-                        )}
+                        className={clsx("relative my-6", (isInvalid || isSolved) && "pointer-events-none")}
                         style={{
                             width: gridWidth * cellSize + (gridWidth - 1) * cellGap,
                             height: gridHeight * cellSize + (gridHeight - 1) * cellGap,
@@ -74,9 +78,9 @@ export default function App() {
                                 <motion.div
                                     key={value}
                                     className={clsx(
-                                        "absolute rounded-md",
-                                        !isInvalid && !isSolved && "bg-neutral-700",
-                                        isInvalid && "bg-red-700",
+                                        "absolute z-10 rounded-md",
+                                        !isInvalid && !isSolved && "cursor-pointer bg-neutral-700",
+                                        isInvalid && "bg-red-400",
                                         isSolved && "bg-green-500",
                                     )}
                                     onClick={() => handleGridClick(index)}
@@ -96,12 +100,14 @@ export default function App() {
                             );
                         })}
                     </div>
-                    <button
-                        className="cursor-pointer rounded-sm border bg-neutral-200 px-4 py-2 hover:bg-neutral-300"
+                    <motion.button
+                        className="cursor-pointer rounded-sm bg-neutral-300 px-4 py-2 hover:bg-neutral-400"
                         onClick={handleRegenerate}
+                        whileHover={{ scale: 1.05 }}
+                        whileTap={{ scale: 0.95 }}
                     >
                         Regenerate
-                    </button>
+                    </motion.button>
                 </div>
             </main>
             <footer></footer>
